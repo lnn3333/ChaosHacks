87e424750b83f444237fd5e98d5fb8602bd4f6aa
ShukaiG <shukai.workmail@gmail.com>
2024-03-02 14:54:01 -0800

    AHHHHHHHHHHHHHHH

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,18 @@
-import { useRef, useEffect } from 'react';
+import { useEffect } from 'react'
 
-interface BoxProps {
-  x: number;
-  y: number;
-}
-
-function Box({ x, y }: BoxProps) {
-  const boxRef = useRef<HTMLDivElement>(null);
-  const animationFrameIdRef = useRef<number>();
-
-  useEffect(() => {
-    const box = boxRef.current;
-    if (!box) return;
-
-    let dy = 0; // Vertical velocity
-    let gravity = 0.2; // Reduced gravity force
-    const ground = window.innerHeight - box.clientHeight; // Ground position
-
-    const fall = () => {
-      // Update position based on velocity
-      y += dy;
-      box.style.transform = `translate(${x}px, ${y}px)`;
-
-      // Apply gravity
-      if (y + (box.clientHeight / 2)< ground) {
-        dy += gravity; // Increase velocity if above ground
-      } else {
-        dy = 0; // Stop falling when hitting the ground
-      }
-
-      // Request next animation frame
-      animationFrameIdRef.current = requestAnimationFrame(fall);
-    };
-
-    // Start falling animation
-    fall();
-
-    // Clean up
-    return () => cancelAnimationFrame(animationFrameIdRef.current!);
-  }, [x, y]); // Re-run effect when x or y changes
-
-  return <div ref={boxRef} style={{ width: '50px', height: '50px', border: '2px solid black', position: 'absolute', backgroundColor: 'transparent' }}></div>;
-}
+import './App.css'
+import { startGame } from './game';
 
 function App() {
-  // Generate initial positions for boxes
-  const boxes = Array.from({ length: 5 }, (_, index) => ({ x: Math.random() * (window.innerWidth - 100) + 50, y: Math.random() * 50 }));
+  useEffect(() => {
+    startGame();
+  }, []);
 
   return (
-    <div style={{ position: 'relative' }}>
-      {boxes.map((box, index) => (
-        <Box key={index} x={box.x} y={box.y} />
-      ))}
+    <div>
+      {}
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/game.js
@@ -0,0 +1,59 @@
+// game.js
+export var box;
+
+export function startGame() {
+    box = new component(30, 30, "red", 80, 75);
+    area.start();
+}
+
+export var area = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);        
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    },    
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+export function component(width, height, color, x, y, type) {
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;    
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.gravity = 0.05;
+    this.gravitySpeed = 0;
+    this.update = function() {
+        ctx = area.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
+    }
+    this.hitBottom = function() {
+        var rockbottom = area.canvas.height - this.height;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+        }
+    }
+}
+
+function updateGameArea() {
+    area.clear();
+    box.newPos();
+    box.update();
+}
