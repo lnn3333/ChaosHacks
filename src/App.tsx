36e2d25825edb623bc0b5400f7ce050b3a44a9f8
ShukaiG <shukai.workmail@gmail.com>
2024-03-02 13:17:38 -0800

    aaaaaah

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import React from 'react';
+import PhysicsSimulation from './physics';
 
 function App() {
-  const [position, setPosition] = useState({ x: 0, y: 0 })
-  const [falling, setFalling] = useState(true)
-
-  useEffect(() => {
-    const box = document.querySelector('.box') as HTMLElement
-    const container = document.querySelector('.container') as HTMLElement
-    const containerRect = container.getBoundingClientRect()
-    
-    const fall = () => {
-      if (position.y < containerRect.bottom - box.clientHeight) {
-        setPosition(prevPosition => ({ ...prevPosition, y: prevPosition.y + 1 })) // Update position
-      } else {
-        setFalling(false) // Stop falling when box reaches bottom
-      }
-    }
-
-    if (falling) {
-      const fallInterval = setInterval(fall, 10)
-      return () => clearInterval(fallInterval)
-    }
-  }, [position, falling])
-
   return (
-    <div className="container">
-      <div className="box" style={{ left: position.x, top: position.y }}></div>
+    <div className="App">
+      <PhysicsSimulation />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
