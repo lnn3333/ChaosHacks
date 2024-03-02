dc34f83c6f4694010a26b4e394e9b5f1a6542d6c
ShukaiG <shukai.workmail@gmail.com>
2024-03-02 13:14:16 -0800

    gravity?

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [falling, setFalling] = useState(true)
+
+  useEffect(() => {
+    const box = document.querySelector('.box') as HTMLElement
+    const container = document.querySelector('.container') as HTMLElement
+    const containerRect = container.getBoundingClientRect()
+    
+    const fall = () => {
+      if (position.y < containerRect.bottom - box.clientHeight) {
+        setPosition(prevPosition => ({ ...prevPosition, y: prevPosition.y + 1 })) // Update position
+      } else {
+        setFalling(false) // Stop falling when box reaches bottom
+      }
+    }
+
+    if (falling) {
+      const fallInterval = setInterval(fall, 10)
+      return () => clearInterval(fallInterval)
+    }
+  }, [position, falling])
 
   return (
     <div className="container">
-      <div className="box"></div> {/* This is the box */}
+      <div className="box" style={{ left: position.x, top: position.y }}></div>
     </div>
   )
 }
